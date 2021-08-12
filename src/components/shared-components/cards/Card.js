95b105e379ae1b73f4d48947cbce47b6d1d47dc5
morgankbender <mkbender@mta.ca>
2021-08-12 16:22:56 -0400

    Removed nonsense comments

--- src/components/shared-components/cards/Card.js
+++ src/components/shared-components/cards/Card.js
@@ -14,10 +14,6 @@ const CardImg = styled.img`
   
 `;
 
-/**
- * This component generates main site navigation, fixed to
- * the top of the screen.
- */
 class Card extends Component {
   /*
    * **************************** Setup ****************************************
@@ -35,32 +31,6 @@ class Card extends Component {
     link: null,
     linkText: null,
   };
-
-  /**
-   * When a nav item is clicked, scroll to element
-   * should be activated, AND mobile menu should be forced
-   * closed (though, it may already be closed if the user
-   * is on a desktop)
-   * @param idTag - id of element the app should scroll
-   * to (include # in param)
-   */
-  handleItemClick(idTag) {
-    scrollToElement(idTag, { align: 'middle' });
-    this.setState({
-      mobileOpen: false,
-    });
-  }
-
-  /**
-   * Called on click event. Toggle state of mobile nav.
-   */
-  toggleMobileNav() {
-    const { mobileOpen } = this.state;
-
-    this.setState({
-      mobileOpen: !mobileOpen,
-    });
-  }
 
   /**
    * *********************** Visual component **********************************
